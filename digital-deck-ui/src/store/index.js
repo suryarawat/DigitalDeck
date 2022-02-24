caa1d5d23b105886529295f43546928098526142
mdangnguyen <nguye35@myumanitoba.ca>
2022-02-24 01:34:07 -0600

    Created basic layout for frontend digital deck
    Removed unnecessary boilerplate codes and files. Added vuex and store. Added initSession to automatically call /new-session when the app loads. Added card deck.
    
    Added a card deck that players can draw a new card from by clicking on it
    
    Added different visuals for the card deck depending on how many cards are left
    
    Removed local testing code. Added proper unit tests for initSession and drawCards
    
    Modified API routes to follow new project structure. Added IN_PROD flag to disable all tests when app is in production.

---
+++ digital-deck-ui/src/store/index.js
@@ -0,0 +1,79 @@
+import { createStore } from 'vuex';
+import axios from 'axios';
+import UnitTests from './../scripts/UnitTests.js';
+
+const URL = 'http://localhost:5000';
+
+export default createStore({
+    state: {
+        sessionId: -1,
+        playerId: -1,
+        playerCards: null,
+        tableCards: null,
+        numCardsInDeck: -1
+    },
+    mutations: {
+        setSessionId(state, sessionId) {
+            state.sessionId = sessionId;
+        },
+
+        setPlayerId(state, playerId) {
+            state.playerId = playerId;
+        },
+
+        setPlayerCards(state, playerCards) {
+            state.playerCards = playerCards;
+        },
+
+        setTableCards(state, table) {
+            state.tableCards = table.cards;
+        },
+
+        setCardsInDeck(state, deck) {
+            state.numCardsInDeck = deck.length;
+        }
+    },
+    actions: {
+        initSession({ commit, state }) {
+            return axios.post(URL + '/session/new', {
+                decks: 1,
+                players: 1,
+                cardsPerPlayer: 1,
+                cardsOnTable: 0
+            }).then((res) => {
+                commit('setSessionId', res.data.sessionId);
+                commit('setPlayerId', res.data.players[0].playerId);
+                commit('setPlayerCards', res.data.players[0].cards);
+                commit('setTableCards', res.data.table);
+                commit('setCardsInDeck', res.data.deck);
+
+                UnitTests.testInitSession(state);
+            }).catch((err) => console.log(err));
+        },
+
+        drawCards({ commit, state }) {
+            let oldPlayerCards = state.playerCards;
+            let oldCardsInDeck = state.numCardsInDeck;
+
+            axios.post(URL + '/player/drawcard', {
+                sessionId: state.sessionId,
+                playerId: state.playerId,
+                numOfCards: 1
+            }).then((res) => {
+                commit('setPlayerCards', res.data.cards);
+                commit('setCardsInDeck', res.data.deck);
+
+                UnitTests.testDrawCards(state, oldPlayerCards, oldCardsInDeck);
+            }).catch((err) => console.log(err));
+        }
+    },
+    getters: {
+        getPlayerCards(state) {
+            return state.playerCards;   // this will automatically track the changes of playerCards so you don't have to watch for it
+        },
+
+        getNumCardsInDeck(state) {
+            return state.numCardsInDeck;
+        }
+    }
+});
