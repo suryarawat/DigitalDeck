8f19e56f3252f0d5a720130dc8fa85b511d4ce89
Hasan Saleem <hasansal-uofm@outlook.com>
2022-02-23 19:36:13 -0600

    ShuffleService File

---
+++ digital-deck-server/services/cardShuffleService.js
@@ -0,0 +1,23 @@
+module.exports = {
+    /**
+     * Shuffles the cards in the deck
+     * 
+     * @param cards The deck of cards to be drawn from
+     * @return {json} The json containing the ids cards currently in the player's hand and the current deck
+     */
+    ShuffleCards: function(cards) {
+      if (cards.length>=1) {
+        for (let i = cards.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * i);
+            let temp = cards[i];
+            cards[i] = cards[j];
+            cards[j] = temp;
+          }
+      } else {
+        throw new Error(
+          `Not enough cards. Cannot Shuffle deck as it has ${cards.length} cards left.`
+        );
+      }      
+      return { cards: cards };
+    }
+  }
