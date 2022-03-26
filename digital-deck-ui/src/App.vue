2a6decfa801a73b51845e079acc2bf93a44cbb69
Surya <srsuryarawat.13@gmail.com>
2022-03-26 15:59:08 -0500

    Final IP change to connect with server service

--- digital-deck-ui/src/App.vue
+++ digital-deck-ui/src/App.vue
@@ -8,7 +8,7 @@ import io from "socket.io-client";
 
 export let api_url =  "http://localhost:5000";
 if ( process.env.NODE_ENV === 'production'){
-    api_url = 'http://10.16.15.162';
+    api_url = 'http://34.132.36.123:5000';
 }
 
 export default {
