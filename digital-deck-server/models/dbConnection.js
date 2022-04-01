0d7bd3874c039d577997f10ff9cce2cecde238cc
Surya <srsuryarawat.13@gmail.com>
2022-04-01 14:03:10 -0500

    Added support for secret in deployment env

--- digital-deck-server/models/dbConnection.js
+++ digital-deck-server/models/dbConnection.js
@@ -1,8 +1,28 @@
 require('dotenv').config({path:'./../digital-deck-server/ENV.env'});
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://app:" + process.env.PASSWORD + "@dd.mkxa1.mongodb.net/DD?retryWrites=true&w=majority";
+const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
+
+var password = '';
+if (process.env.NODE_ENV != 'production'){
+    password = process.env.PASSWORD;
+}
+else{
+    // Instantiates a client
+    const client = new SecretManagerServiceClient();
+
+    async function accessSecretVersion() {
+        const [version] = await client.accessSecretVersion({
+            name: "MongoDBPassword",
+        });
+        // Extract the password as a string.
+        password = version.payload.data.toString();
+    }
+    accessSecretVersion();
+}
+const uri = "mongodb+srv://app:" + password + "@dd.mkxa1.mongodb.net/DD?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 client.connect();
 const database = client.db('DD');
 const sessionData = database.collection('sessions');
 module.exports = sessionData;
+
