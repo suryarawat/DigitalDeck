b7e17f6968bb3a4d08e8e7bb4733f1ab6a3b9d59
Surya <srsuryarawat.13@gmail.com>
2022-04-02 23:22:22 -0500

    Hard coded db password

--- digital-deck-server/models/dbConnection.js
+++ digital-deck-server/models/dbConnection.js
@@ -35,8 +35,7 @@ async function readPassword(){
 * Creates a mongo database connection
 */
 async function getDB(){
-    const password = await readPassword();
-    const uri = "mongodb+srv://app:" + password + "@dd.mkxa1.mongodb.net/DD?retryWrites=true&w=majority";
+    const uri = "mongodb+srv://app:u5rzwmYi8GTVvf-@dd.mkxa1.mongodb.net/DD?retryWrites=true&w=majority";
     const client = new MongoClient(uri);
     const connection = await client.connect();
     const database = client.db('DD');
