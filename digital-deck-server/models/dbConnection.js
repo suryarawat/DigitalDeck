878bffa7f296273b8fed0c5a7fa7348c3897f9e9
Surya <srsuryarawat.13@gmail.com>
2022-04-02 21:12:55 -0500

    refactored password reading code

--- digital-deck-server/models/dbConnection.js
+++ digital-deck-server/models/dbConnection.js
@@ -2,29 +2,40 @@ require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
 
-var password = '';
-if (process.env.NODE_ENV != 'production' || process.env.CI) {
-    password = process.env.PASSWORD;
-}
-else{
-    // Instantiates a client
-    const client = new SecretManagerServiceClient();
-    const name = 'projects/628328056407/secrets/MongoDBPassword/versions/1';
-    console.log('name');
-    async function accessSecretVersion() {
-        const [version] = await client.accessSecretVersion({
-            name: name,
-        });
-        // Extract the password as a string.
-        password = version.payload.data.toString();
+/*
+* Reads the password from either .env file or secrets in GCP
+*/
+async function readPassword(){
+    var password = "";
+    if (process.env.NODE_ENV != 'production' || process.env.CI) {
+        password = process.env.PASSWORD;
     }
-    accessSecretVersion();
-}
-if (password != "") {
-    console.log('password found!');
+    else{
+        // Instantiates a client
+        const client = new SecretManagerServiceClient();
+        const name = 'projects/628328056407/secrets/MongoDBPassword/versions/1';
+        console.log(name);
+        async function accessSecretVersion() {
+            const [version] = await client.accessSecretVersion({
+                name: name,
+            });
+            // Extract the password as a string.
+            console.log(version.payload.data.toString());
+            password = version.payload.data.toString();
+        }
+        accessSecretVersion();
+    }
+    if (password != "") {
+        console.log('password found!');
+    }
+    return password;
 }
 
+/*
+* Creates a mongo database connection
+*/
 async function getDB(){
+    const password = await readPassword();
     const uri = "mongodb+srv://app:" + password + "@dd.mkxa1.mongodb.net/DD?retryWrites=true&w=majority";
     const client = new MongoClient(uri);
     const connection = await client.connect();
